e2dba56c06161edaa71eddb444774d1942db76d0
xxjandroxx14 <gabrielferrera726@gmail.com>
2025-12-12 17:59:11 +0800

    Add project files.

---
+++ FerreraTTEProblem1/Form1.Designer.vb
@@ -0,0 +1,201 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBoxTitle = New System.Windows.Forms.TextBox()
+        Me.TextBoxArtist = New System.Windows.Forms.TextBox()
+        Me.TextBoxDuration = New System.Windows.Forms.TextBox()
+        Me.ComboBoxGenre = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.ButtonAdd = New System.Windows.Forms.Button()
+        Me.ButtonUpdate = New System.Windows.Forms.Button()
+        Me.ButtonDelete = New System.Windows.Forms.Button()
+        Me.ButtonLoad = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBoxId = New System.Windows.Forms.TextBox()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TextBoxTitle
+        '
+        Me.TextBoxTitle.Location = New System.Drawing.Point(177, 74)
+        Me.TextBoxTitle.Name = "TextBoxTitle"
+        Me.TextBoxTitle.Size = New System.Drawing.Size(100, 20)
+        Me.TextBoxTitle.TabIndex = 0
+        '
+        'TextBoxArtist
+        '
+        Me.TextBoxArtist.Location = New System.Drawing.Point(177, 128)
+        Me.TextBoxArtist.Name = "TextBoxArtist"
+        Me.TextBoxArtist.Size = New System.Drawing.Size(100, 20)
+        Me.TextBoxArtist.TabIndex = 1
+        '
+        'TextBoxDuration
+        '
+        Me.TextBoxDuration.Location = New System.Drawing.Point(177, 180)
+        Me.TextBoxDuration.Name = "TextBoxDuration"
+        Me.TextBoxDuration.Size = New System.Drawing.Size(100, 20)
+        Me.TextBoxDuration.TabIndex = 2
+        '
+        'ComboBoxGenre
+        '
+        Me.ComboBoxGenre.FormattingEnabled = True
+        Me.ComboBoxGenre.Items.AddRange(New Object() {"Pop", "Rock", "Jazz", "Hip-Hop", "Classical"})
+        Me.ComboBoxGenre.Location = New System.Drawing.Point(156, 232)
+        Me.ComboBoxGenre.Name = "ComboBoxGenre"
+        Me.ComboBoxGenre.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBoxGenre.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(78, 74)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(61, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Track Title:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(78, 128)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(30, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Artist"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(72, 240)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(36, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Genre"
+        '
+        'ButtonAdd
+        '
+        Me.ButtonAdd.Location = New System.Drawing.Point(336, 320)
+        Me.ButtonAdd.Name = "ButtonAdd"
+        Me.ButtonAdd.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonAdd.TabIndex = 7
+        Me.ButtonAdd.Text = "Add"
+        Me.ButtonAdd.UseVisualStyleBackColor = True
+        '
+        'ButtonUpdate
+        '
+        Me.ButtonUpdate.Location = New System.Drawing.Point(438, 320)
+        Me.ButtonUpdate.Name = "ButtonUpdate"
+        Me.ButtonUpdate.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonUpdate.TabIndex = 8
+        Me.ButtonUpdate.Text = "Update"
+        Me.ButtonUpdate.UseVisualStyleBackColor = True
+        '
+        'ButtonDelete
+        '
+        Me.ButtonDelete.Location = New System.Drawing.Point(533, 320)
+        Me.ButtonDelete.Name = "ButtonDelete"
+        Me.ButtonDelete.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonDelete.TabIndex = 9
+        Me.ButtonDelete.Text = "Delete"
+        Me.ButtonDelete.UseVisualStyleBackColor = True
+        '
+        'ButtonLoad
+        '
+        Me.ButtonLoad.Location = New System.Drawing.Point(644, 320)
+        Me.ButtonLoad.Name = "ButtonLoad"
+        Me.ButtonLoad.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonLoad.TabIndex = 10
+        Me.ButtonLoad.Text = "Load"
+        Me.ButtonLoad.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(308, 32)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(449, 263)
+        Me.DataGridView1.TabIndex = 11
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(78, 180)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(47, 13)
+        Me.Label4.TabIndex = 12
+        Me.Label4.Text = "Duration"
+        '
+        'TextBoxId
+        '
+        Me.TextBoxId.Location = New System.Drawing.Point(177, 22)
+        Me.TextBoxId.Name = "TextBoxId"
+        Me.TextBoxId.Size = New System.Drawing.Size(100, 20)
+        Me.TextBoxId.TabIndex = 13
+        Me.TextBoxId.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.TextBoxId)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.ButtonLoad)
+        Me.Controls.Add(Me.ButtonDelete)
+        Me.Controls.Add(Me.ButtonUpdate)
+        Me.Controls.Add(Me.ButtonAdd)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ComboBoxGenre)
+        Me.Controls.Add(Me.TextBoxDuration)
+        Me.Controls.Add(Me.TextBoxArtist)
+        Me.Controls.Add(Me.TextBoxTitle)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBoxTitle As TextBox
+    Friend WithEvents TextBoxArtist As TextBox
+    Friend WithEvents TextBoxDuration As TextBox
+    Friend WithEvents ComboBoxGenre As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents ButtonAdd As Button
+    Friend WithEvents ButtonUpdate As Button
+    Friend WithEvents ButtonDelete As Button
+    Friend WithEvents ButtonLoad As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBoxId As TextBox
+End Class
---
+++ FerreraTTEProblem1/Form1.vb
@@ -0,0 +1,153 @@
+﻿Imports MySql.Data.MySqlClient
+Imports System.Data
+Public Class Form1
+
+    Private Const CONN_STRING As String = "server=localhost; userid=root; password=root;database=musicstudio_db;"
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ButtonLoad_Click(Nothing, EventArgs.Empty)
+    End Sub
+
+    Private Sub ButtonAdd_Click(sender As Object, e As EventArgs) Handles ButtonAdd.Click
+        If String.IsNullOrWhiteSpace(TextBoxTitle.Text) OrElse String.IsNullOrWhiteSpace(TextBoxTitle.Text) OrElse String.IsNullOrWhiteSpace(TextBoxArtist.Text) OrElse String.IsNullOrWhiteSpace(TextBoxDuration.Text) Then
+            MessageBox.Show("Please enter a value in all required fields!", "Missing Value", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Return
+        End If
+
+        If IsDuplicateRecord(TextBoxTitle.Text, TextBoxArtist.Text, TextBoxDuration.Text) Then
+            MessageBox.Show("A record with the same Title, Author, and Category already exists.", "Duplicate Record", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Return
+        End If
+
+
+        Dim query As String = "INSERT INTO musicstudio_db.tracks_tbl(title,artist,duration,genre) VALUES ( @title, @artist, @duration,@genre);"
+        Try
+            Using conn As New MySqlConnection(CONN_STRING)
+                conn.Open()
+                Using cmd As New MySqlCommand(query, conn)
+
+                    cmd.Parameters.AddWithValue("@title", TextBoxTitle.Text)
+                    cmd.Parameters.AddWithValue("@artist", TextBoxArtist.Text)
+                    cmd.Parameters.AddWithValue("@duration", TextBoxDuration.Text)
+                    cmd.Parameters.AddWithValue("@genre", ComboBoxGenre.Text)
+                    cmd.ExecuteNonQuery()
+                    MessageBox.Show("Record insert successfully!")
+
+                    TextBoxTitle.Clear()
+                    TextBoxArtist.Clear()
+                    TextBoxDuration.Clear()
+
+
+                End Using
+            End Using
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+    Private Function IsDuplicateRecord(title As String, author As String, category As String) As Boolean
+        Dim query As String = "SELECT COUNT(id) FROM musicstudio_db.tracks_tbl WHERE title = @title AND Artist = @artist;"
+        Try
+            Using conn As New MySqlConnection(CONN_STRING)
+                conn.Open()
+                Using cmd As New MySqlCommand(query, conn)
+                    cmd.Parameters.AddWithValue("@title", title)
+                    cmd.Parameters.AddWithValue("@artist", author)
+
+
+                    Dim count As Integer = Convert.ToInt32(cmd.ExecuteScalar())
+                    Return count > 0 '
+                End Using
+            End Using
+        Catch ex As Exception
+            MsgBox("Error checking for duplicates: " & ex.Message)
+            Return True
+        End Try
+    End Function
+
+
+
+    Private Sub ButtonLoad_Click(sender As Object, e As EventArgs) Handles ButtonLoad.Click
+        Dim query As String = "SELECT * FROM musicstudio_db.tracks_tbl;"
+        Try
+            Using conn As New MySqlConnection(CONN_STRING)
+                Dim adapter As New MySqlDataAdapter(query, conn)
+                Dim table As New DataTable()
+                adapter.Fill(table)
+                DataGridView1.DataSource = table
+                If DataGridView1.Columns.Contains("id") Then
+                    DataGridView1.Columns("id").Visible = False
+                End If
+            End Using
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+        If e.RowIndex >= 0 Then
+            Dim selectedRow As DataGridViewRow = DataGridView1.Rows(e.RowIndex)
+            TextBoxTitle.Text = selectedRow.Cells("title").Value.ToString()
+            TextBoxArtist.Text = selectedRow.Cells("artist").Value.ToString()
+            TextBoxDuration.Text = selectedRow.Cells("duration").Value.ToString()
+            ComboBoxGenre.Text = selectedRow.Cells("genre").Value.ToString()
+            TextBoxId.Text = selectedRow.Cells("id").Value.ToString()
+        End If
+    End Sub
+
+    Private Sub ButtonUpdate_Click(sender As Object, e As EventArgs) Handles ButtonUpdate.Click
+        Dim query As String = "UPDATE musicstudio_db.tracks_tbl SET title = @title, artist = @artist, duration = @duration, genre= @genre WHERE id = @id;"
+        If String.IsNullOrWhiteSpace(TextBoxId.Text) Then
+            MessageBox.Show("Please select a record to update first by double-clicking the grid.")
+            Return
+        End If
+        If String.IsNullOrWhiteSpace(TextBoxTitle.Text) Then
+            MessageBox.Show("Please enter a title value before updating!", "Missing Value", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            TextBoxTitle.Focus()
+            Return
+        End If
+        Try
+            Using conn As New MySqlConnection(CONN_STRING)
+                conn.Open()
+                Using cmd As New MySqlCommand(query, conn)
+                    cmd.Parameters.AddWithValue("@id", CInt(TextBoxId.Text))
+                    cmd.Parameters.AddWithValue("@title", TextBoxTitle.Text)
+                    cmd.Parameters.AddWithValue("@author", TextBoxArtist.Text)
+                    cmd.Parameters.AddWithValue("@category", TextBoxDuration.Text)
+                    cmd.Parameters.AddWithValue("@availability", ComboBoxGenre.Text)
+                    cmd.ExecuteNonQuery()
+                    MessageBox.Show("Record updated successfully!")
+                End Using
+            End Using
+        Catch ex As Exception
+            MsgBox("Error updating record: " & ex.Message)
+        End Try
+    End Sub
+
+    Private Sub ButtonDelete_Click(sender As Object, e As EventArgs) Handles ButtonDelete.Click
+        Dim query As String = "DELETE FROM musicstudio_db.tracks_tbl WHERE id = @id;"
+        If String.IsNullOrWhiteSpace(TextBoxId.Text) Then
+            MessageBox.Show("Please select a record to delete first by double-clicking the grid.")
+            Return
+        End If
+        If MessageBox.Show("Are you sure you want to delete this record?", "Confirm Delete", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) = DialogResult.No Then
+            Return
+        End If
+        Try
+            Using conn As New MySqlConnection(CONN_STRING)
+                conn.Open()
+                Using cmd As New MySqlCommand(query, conn)
+                    cmd.Parameters.AddWithValue("@id", CInt(TextBoxId.Text))
+                    cmd.ExecuteNonQuery()
+                    MessageBox.Show("Record deleted successfully!")
+                    TextBoxId.Clear()
+                    TextBoxTitle.Clear()
+                    TextBoxArtist.Clear()
+                    TextBoxDuration.Clear()
+                    ButtonLoad_Click(Nothing, EventArgs.Empty)
+                End Using
+            End Using
+        Catch ex As Exception
+            MsgBox("Error deleting record: " & ex.Message)
+        End Try
+    End Sub
+
+    End Class
